17777667b816991e2f093deb71d6beacc1162bbb
HossainRafi <hossainrafi71@gmail.com>
2022-11-27 20:27:47 +0600

    exchange done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
-import ColorModeSwitcher from "./ColorModeSwitcher";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ColorModeScript />
     <ChakraProvider theme={theme}>
-      <ColorModeSwitcher />
       <App />
     </ChakraProvider>
   </React.StrictMode>
