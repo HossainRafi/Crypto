880fcfe92b845a0c7adc2d871b2990b902768065
HossainRafi <hossainrafi71@gmail.com>
2022-11-27 19:16:40 +0600

    component imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { Route, Router, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Coins from "./components/Coins";
+import Exchanges from "./components/Exchanges";
+import CoinDetails from "./components/CoinDetails";
+
 function App() {
   return (
     <div>
-      <h1>Hello Everyone</h1>
-      <h1>
-        This Project Contains The Basic Setup Of A New React Project. You Need
-        To Change Only The Title & Favicon Of This Project
-      </h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/coins" element={<Coins />} />
+          <Route path="/exchanges" element={<Exchanges />} />
+          <Route path="/coin/:id" element={<CoinDetails />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
