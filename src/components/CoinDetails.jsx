4046354e480a2d5dc86065de877f9eaa4d117633
HossainRafi <hossainrafi71@gmail.com>
2022-11-27 21:42:09 +0600

    chart done

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,21 +1,4 @@
-import {
-  Badge,
-  Box,
-  Button,
-  Container,
-  HStack,
-  Image,
-  Progress,
-  Radio,
-  RadioGroup,
-  Stat,
-  StatArrow,
-  StatHelpText,
-  StatLabel,
-  StatNumber,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
