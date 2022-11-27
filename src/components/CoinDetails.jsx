2ede330d33c75be81b31c17825501d283366ce3b
HossainRafi <hossainrafi71@gmail.com>
2022-11-27 21:18:24 +0600

    loading work

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  HStack,
+  Image,
+  Progress,
+  Radio,
+  RadioGroup,
+  Stat,
+  StatArrow,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { server } from "../index";
+import Chart from "./Chart";
+import ErrorComponent from "./ErrorComponent";
+import Loader from "./Loader";
 
 const CoinDetails = () => {
-  return (
-    <div>CoinDetails</div>
-  )
-}
+  const [coin, setCoin] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [currency, setCurrency] = useState("inr");
 
-export default CoinDetails
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Box width={"full"} borderWidth={1}>
+            
+            </Box>
+            </>
+    </Container>
+  );
+};
+
+export default CoinDetails;
